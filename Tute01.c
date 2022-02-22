6628d972eea17f77c051b993c48b85b69a353e0b
it21322280 <99573071+it21322280@users.noreply.github.com>
2022-02-22 06:14:08 +0000

    I have done the first question

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
+
+  int m1,m2;
+  float avg;
+
+  printf("Enter the Module 01 Mark : ");
+  scanf("%d", &m1);
+  printf("Enter the Module 02 Mark : ");
+  scanf("%d", &m2);
   
+  avg=(m1+m2)/2.0;
+
+  printf("Average Mark is %.2f", avg);
   return 0;
 }
 
