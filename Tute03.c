e6ece78eab3dc65a8d16093e5a7006ca87f66b73
it21322280 <99573071+it21322280@users.noreply.github.com>
2022-03-02 20:11:57 +0000

    I have done the third question

--- Tute03.c
+++ Tute03.c
@@ -13,6 +13,18 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+
+  int n,sum=0;
+
+  printf("Enter the number: ");
+  scanf("%d", &n);
+  
+   while(n>=0){
+     sum = sum + n;
+     n--;
+   }
+
+  printf("Sum is %d", sum);
   
   return 0;
 }
